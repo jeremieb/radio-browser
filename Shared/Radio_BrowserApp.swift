11324fa7593cd960649c6b12d1b38eeb5b1974de
Jeremie <jeremieberduck@icloud.com>
2021-04-08 16:58:06 +0200

    Project initialized

---
+++ Shared/Radio_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Radio_BrowserApp.swift
+//  Shared
+//
+//  Created by Jeremie Berduck on 08/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Radio_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
